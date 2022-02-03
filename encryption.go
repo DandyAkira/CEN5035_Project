ca4ce308ff8dc1e5b34b6883d79c1044d16235f2
ChengPlus7 <77910068+TR280@users.noreply.github.com>
2022-02-03 12:27:41 -0500

    Add files via upload

---
+++ encryption.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+//返回一个32位md5加密后的字符串
+func Md5Encode32(password string) string {
+	h := md5.New()
+	h.Write([]byte(password))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+//返回一个16位md5 加密后的字符串
+func MD5Encode16(password string) string {
+	return MD5Encode16(password)[8:24]
+}
