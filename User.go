f3c520146d28f4b44fef49c8d22f76e2304c77af
程家琪 <jiaqicheng@ufl.edu>
2022-02-27 13:50:17 -0500

    ver2

--- User.go
+++ User.go
@@ -1,50 +1,69 @@
-package main
+package controller
 
 import (
-	"net"
+	"GatorChat/global"
+	"GatorChat/model"
+	"GatorChat/service"
+	"GatorChat/utils"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
-type User struct {
-	ID   int
-	Name string
-	Addr string
-	Ch   chan string
-	conn net.Conn
-
-	connectedServer *Server
-}
-
-// 创建新用户API
-func NewUser(conn net.Conn, server *Server, userID int) *User {
-	userAddr := conn.RemoteAddr().String()
-
-	user := &User{
-		ID:              userID,
-		Name:            userAddr,
-		Addr:            userAddr,
-		Ch:              make(chan string),
-		conn:            conn,
-		connectedServer: server,
+func Login(writer http.ResponseWriter, request *http.Request) {
+	_ = request.ParseForm()
+	email := request.PostForm.Get("email")
+	fmt.Println("Receive Login Request from: ", email)
+	password := request.PostForm.Get("password")
+	user, err := service.UserService{}.Login(email, password)
+	if err != nil {
+		global.ResponseFail(writer, err.Error())
+	} else {
+		global.ResponseOk(writer, user, "Login Success")
 	}
-
-	// 监听channel
-	go user.ListenMessage()
-
-	return user
 }
 
-func (thisUser *User) Push2Client(msg string) {
-	thisUser.conn.Write([]byte(msg))
+func Register(writer http.ResponseWriter, request *http.Request) {
+
+	_ = request.ParseForm()
+	email := request.PostForm.Get("email")
+	fmt.Println("Receive Register Request from: ", email)
+	if len(email) <= 0 || !strings.Contains(email, "@") {
+		global.ResponseFail(writer, "illegal email")
+		return
+	}
+	password := request.PostForm.Get("password")
+	if len(password) <= 0 {
+		global.ResponseFail(writer, "Illegal Password")
+		return
+	}
+	nickname := request.PostForm.Get("nickname")
+	if len(nickname) == 0 {
+		nickname = "user" + strconv.Itoa(rand.Intn(10000))
+	}
+	avatar := ""
+	sex := model.SEX_UNKNOW
+
+	user, err := service.UserService{}.Register(email, password, nickname, avatar, sex)
+
+	if err != nil {
+		global.ResponseFail(writer, err.Error())
+	} else {
+		global.ResponseOk(writer, user.Email, "Register Success at "+user.Createat.String())
+	}
 }
 
-func (thisUser *User) DoMessage(msg string) {
-	thisUser.connectedServer.Broadcast(thisUser, msg)
-}
-
-// 监听当前user channel
-func (thisUser *User) ListenMessage() {
-	for {
-		msg := <-thisUser.Ch
-		thisUser.Push2Client(msg + "\n")
+func GetUser(writer http.ResponseWriter, request *http.Request) {
+	var user model.User
+	if err := utils.Bind(request, &user); err != nil {
+		global.ResponseFail(writer, err.Error())
+	}
+	user, err := service.UserService{}.GetUserById(user.Id)
+	if err != nil {
+		global.ResponseFail(writer, err.Error())
+	} else {
+		global.ResponseOk(writer, user, "Get User OK")
 	}
 }
