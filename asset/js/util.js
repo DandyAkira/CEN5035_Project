8831fd44ff69b45ccb58a35c9a56b052227f49a2
kalmanxiong <ruoyangxiong@163.com>
2022-04-01 18:29:45 -0400

    style(util):delete useless comment

--- asset/js/util.js
+++ asset/js/util.js
@@ -196,10 +196,10 @@ Core.prototype.parseUri = function (url) {
 }
 Core.prototype.parseQuery = function (name) {
 
-	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-	var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
 	if (r != null) return decodeURI(unescape(r[2]));
-	return null; //返回参数值
+	return null;
 }
 
 Core.prototype.isemail = function (email) {
@@ -210,7 +210,7 @@ Core.prototype.ismobile = function (mobile) {
 }
 
 Core.prototype.test = function (reg, data) {
-	var reg = new RegExp(reg); //构造一个含有目标参数的正则表达式对象
+	var reg = new RegExp(reg);
 	return reg.test(data)
 }
 window.util = new Core();
