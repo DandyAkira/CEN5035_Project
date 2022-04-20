3249f508f360144ae6ebd8468d8704bca9501a04
ChengPlus7 <77910068+TR280@users.noreply.github.com>
2022-04-19 21:06:24 -0400

    Delete response1.go

--- backend/model/response/response1.go
+++
@@ -1,9 +0,0 @@
-package response
-
-type Res struct {
-	Code  int         `json:"code"`
-	Msg   string      `json:"msg"`
-	Data  interface{} `json:"data,omitempty"`
-	Rows  interface{} `json:"rows,omitempty"`
-	Total interface{} `json:"total,omitempty"`
-}
